dc9e85133ad27c95a4ffffd8c0f5e83f06e10dac
dwane <dgard02@gmail.com>
2016-05-19 12:01:14 +1000

    added commenting to rail fence

--- rail_fence.py
+++ rail_fence.py
@@ -1,6 +1,12 @@
 import collections
 
 def fence(cipher_text, key_size):
+    '''
+    used by decode for its processing
+    :param cipher_text:
+    :param key_size:
+    :return:
+    '''
     # Create a list of lists with null values to be filled
     fence = [[None] * len(cipher_text) for n in range(key_size)]
 
@@ -21,31 +27,48 @@ def fence(cipher_text, key_size):
     return [x for rail in fence for x in rail if x is not None]
 
 def encode(text, n):
+    '''
+    Encode text using a railfence cipher with a key of n length
+    :param text:
+    :param n:
+    :return:
+    '''
     return ''.join(fence(text, n))
 
 def decode(text, n):
+    '''
+    decode text using a railfence cipher with a key of n length
+    :param text:
+    :param n:
+    :return:
+    '''
     rng = range(len(text))
     pos = fence(rng, n)
     return ''.join(text[pos.index(n)] for n in rng)
 
-# 10 chars dontattack
-# z = encode('DONTATTACKDONTATTACKDONTATTACKDONTATTACKDONTATTACKDONTATTACKDONTATTACKDONTATTACKDONTATTACK', 10)
-# print(z) # ACTWTAKA.ANT.D
 
-for x in range(2,50,1):
-    print(x)
-    cipher_text = "S_   ltes e__owesft4ya'h r_ernadinhohn_hstfeamion coo iost  lhrooidskeutsio t,aPeeut_eemlc tmkhegi_wschoool31neOen Cbale4h s tee_  oi_r yjnsr  iat_.>dslu}4 nd asthsnCg- it_ Misdirection_tCaeesa Oe1elr__firiOR_lelsmk_hlabsfkabM{fbbliuec_p  eiecn P1oaubco a_ite_headm34rebihchtHo4c"
-    # print(collections.Counter(cipher_text))
-    for char in range(0,len(cipher_text), 1):
-        cipher_text = "S_   ltes e__owesft4ya'h r_ernadinhohn_hstfeamion coo iost  lhrooidskeutsio t,aPeeut_eemlc tmkhegi_wschoool31neOen Cbale4h s tee_  oi_r yjnsr  iat_.>dslu}4 nd asthsnCg- it_ Misdirection_tCaeesa Oe1elr__firiOR_lelsmk_hlabsfkabM{fbbliuec_p  eiecn P1oaubco a_ite_headm34rebihchtHo4c"
-        cipher_text = cipher_text[:char] + 'x' + cipher_text[char:]
+if __name__ == '__main__':
+    # 10 chars dontattack
+    z = encode('DONTATTACKDONTATTACKDONTATTACKDONTATTACKDONTATTACKDONTATTACKDONTATTACKDONTATTACKDONTATTACK', 10)
+    print(z) # ACTWTAKA.ANT.D
 
-        y = decode(cipher_text, x)
+    y = decode(z, 10)
+    print(y)
+    for x in range(2, 20, 1):
+        print('\n')
+        print(x)
 
-        # print(y) # ATTACK.AT.DAWN
-        if '1{' in y:
-            print(y +'\n') # ATTACK.AT.DAWN\
+        cipher_text = "S_   ltes e__owesft4ya'h r_ernadinhohn_hstfeamion coo iost  lhrooidskeutsio t,aPeeut_eemlc tmkhegi_wschoool31neOen Cbale4h s tee_  oi_r yjnsr  iat_.>dslu}4 nd asthsnCg\  it_ Misdirection_tCaeesa Oe1elr__firiOR_lelsmk_hlabsfkabM{fbbliuec_p  eiecn P1oaubco a_ite_headm34rebihchtHo4c"
 
-            # exit()
+        # for each in range(1,28,1):
+        fraction = (int(len(cipher_text)/2
+                        ))
+        cipher_text = cipher_text[:fraction], cipher_text[fraction:]
+
+        # print(collections.Counter(cipher_text))
+        for each_cipher_text in cipher_text:
+            y = decode(cipher_text, x)
+            if '3441' in y:
+                print(y) # ATTACK.AT.DAWN\
 
 
