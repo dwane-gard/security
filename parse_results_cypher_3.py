55ccbd92cdb9fcb0a21200fed9af8a7ae48d54e8
dwane <dgard02@gmail.com>
2016-06-22 10:56:59 +1000

    added a parser to analyse resutls quickly

--- parse_results_cypher_3.py
+++ parse_results_cypher_3.py
@@ -6,18 +6,18 @@ with open('10results.txt', 'r') as results:
 best_ic = 0.05
 
 for each in results:
-    try:
+
         ic = each.split('|')[2]
-        # print(ic)
-        # print('*'*10)
+        print(ic)
+        print('error reading')
+
 
         if float(ic) > best_ic:
             print(ic)
             best_ic = ic
-    except:
-        print(ic)
-        print('error reading')
-        pass
+
+
+
 
 
 print(best_ic)
