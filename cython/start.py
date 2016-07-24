53bf8186674728c5aa89c0b1cb868f15b38ed225
Dwane <dgard02@gmail.com>
2016-07-24 10:39:53 +1000

    added multithread

--- cython/start.py
+++ cython/start.py
@@ -45,7 +45,7 @@ class Run:
         #self.brute = itertools.product(self.alphabet, repeat=key_len)
         # self.brute = [self.pre_analysis(), ]
         self.decoder = Decode(self.cipher_text)
-        self.pre_analysis()
+        self.ze_pre_analysis = self.pre_analysis()
         self.transDecode = None
 
     def pre_analysis(self):
@@ -62,104 +62,25 @@ class Run:
             j += 1
 
         return messages
-        # Build the key from the analysis
-        # w = 0
-        # while w < self.key_len:
-        #     key += messages[w].plain_texts[0].shift
-        #     w += 1
-        #
-        # return key
-
-        # j = 0
-        # for each in itertools.product(range(0, 1, 1), repeat=self.key_len):
-        #     key = ''
-        #     for each_char in list(itertools.cycle(each)):
-        #         if each_char is None:
-        #             key += '.'
-        #         else:
-        #             key += messages[j].plain_texts[each_char].shift
-        #             j += 1
-        #     yield key
-
-
 
     def start_combination(self):
-        ''' combination cipher '''
-        pre_analysis = self.pre_analysis()
+        ''' Multi Thread '''
+        q = multiprocessing.Queue(maxsize=50)
+        jobs = []
 
-        # build a generator of possible keys
-        for each in itertools.product([0,1], repeat=self.key_len):
-            j = 0
-            key = ''
-            for each_char in each:
-                if each_char is None:
-                    key += '.'
-                else:
-                    key += pre_analysis[j].plain_texts[each_char].shift
-                    j += 1
+        # Create workers
+        for i in range(0, multiprocessing.cpu_count(), 1):
+            p = multiprocessing.Process(target=self.combination_cipher_worker, args=(q,))
+            p.start()
+            jobs.append(p)
 
-            # using each key test it
-            each_key = key
-            print(each_key)
-            plain_text, key = self.decoder.runner(each_key)
-            chiSquare = ChiSquare(plain_text)
-            ic = chiSquare.ic
-            print('[-] IC: %s' % str(ic))
-            print('[-] CHI: %s' % str(chiSquare.chi_result))
-            if chiSquare.chi_result < 100:
-                print(key)
-                print('[+] Found a possible key running diagram analysis')
+        # Feed items into the queue
+        for each_item in itertools.product([0,1], repeat=self.key_len):
+            q.put(each_item)
 
-                # test the resulting plain text as a transposition cipher
-                self.transDecode = Decode(plain_text)
-                for each_degree in range(2, 50):
-                    # print('[-] Running %d degree' % each_degree)
-                    dia = Dia(plain_text, each_degree)
-                    dia.permutation()
-                    dia.run()
-                    if dia.key is not None:
-                        print(dia.key)
-                        trans_plain_text, trans_key = self.transDecode.permutation(dia.key)
-
-                        # Look For words
-                        wordSearch = WordSearch()
-                        words_len = wordSearch.run(trans_plain_text)
-                        print(key)
-                        print(trans_plain_text)
-                        print(words_len)
-                        if words_len > 1:
-                            print(words_len)
-                            with open('combination_cipher_result.txt', 'a') as results_file:
-                                results_file.write('%s | %s | %s' % (str(key), str(plain_text), str(words_len)))
-
-                        ''' Single Thread '''
-                        # for each_trans_key in transpostional_keys:
-                        #     plain_text, each_trans_key = self.transDecode.permutation(each_trans_key)
-                        #
-                        #     wordSearch = WordSearch()
-                        #     words_len = wordSearch.run(plain_text)
-                        #     print(key)
-                        #     print(plain_text)
-                        #     print(words_len)
-
-
-                        ''' Multi Thread '''
-                        # q = multiprocessing.Queue(maxsize=50)
-                        # jobs = []
-                        #
-                        # # Create workers
-                        # for i in range(0, multiprocessing.cpu_count(), 1):
-                        #     p = multiprocessing.Process(target=self.combination_cipher_worker, args=(q,))
-                        #     p.start()
-                        #     jobs.append(p)
-                        #
-                        # # Feed items into the queue
-                        # for each_item in transpostional_keys:
-                        #     q.put(each_item)
-                        #
-                        # # Wait for each worker to finish before continueing
-                        # for each_job in jobs:
-                        #     each_job.join()
+        # Wait for each worker to finish before continueing
+        for each_job in jobs:
+            each_job.join()
 
     def combination_cipher_worker(self, q):
         while True:
@@ -167,22 +88,57 @@ class Run:
                 time.sleep(1)
             try:
                 obj = q.get(timeout=1)
-                plain_text, key = self.transDecode.permutation(obj)
-
-                wordSearch = WordSearch()
-                words_len = wordSearch.run(plain_text)
-                print(key)
-                print(words_len)
-                if words_len > 1:
-                    print(words_len)
-                    with open('combination_cipher_result.txt', 'a') as results_file:
-                        results_file.write('%s | %s | %s' % (str(key), str(plain_text), str(words_len)))
+                self.combination_cipher(obj)
             except:
                 # print('[!] run finished')
                 break
         # print('ending worker')
         return
 
+    def combination_cipher(self, pre_key):
+        ''' combination cipher '''
+        j = 0
+        key = ''
+        for each_char in pre_key:
+            if each_char is None:
+                key += '.'
+            else:
+                key += self.ze_pre_analysis[j].plain_texts[each_char].shift
+                j += 1
+
+        # using each key test it
+        each_key = key
+        print(each_key)
+        plain_text, key = self.decoder.runner(each_key)
+        chiSquare = ChiSquare(plain_text)
+        ic = chiSquare.ic
+        print('[-] IC: %s' % str(ic))
+        print('[-] CHI: %s' % str(chiSquare.chi_result))
+        if chiSquare.chi_result < 100:
+            print(key)
+            print('[+] Found a possible key running diagram analysis')
+
+            # test the resulting plain text as a transposition cipher
+            self.transDecode = Decode(plain_text)
+            for each_degree in range(2, 50):
+                # print('[-] Running %d degree' % each_degree)
+                dia = Dia(plain_text, each_degree)
+                dia.permutation()
+                dia.run()
+                if dia.key is not None:
+                    print(dia.key)
+                    trans_plain_text, trans_key = self.transDecode.permutation(dia.key)
+
+                    # Look For words
+                    wordSearch = WordSearch()
+                    words_len = wordSearch.run(trans_plain_text)
+                    print(key)
+                    print(trans_plain_text)
+                    print(words_len)
+                    if words_len > 1:
+                        print(words_len)
+                        with open('combination_cipher_result.txt', 'a') as results_file:
+                            results_file.write('%s | %s | %s' % (str(key), str(plain_text), str(words_len)))
 
     def start_simple_substitution(self):
         ''' MultiCore '''
@@ -204,7 +160,6 @@ class Run:
             each_job.join()
 
     def worker(self, q):
-
         while True:
             if q.empty():
                 time.sleep(1)
@@ -306,8 +261,7 @@ if __name__ == '__main__':
     MWZCFHIOTBQKFDHOXZFUALTOGHLCPVDSESWVWYPQSEWNVUERPRFQZFHVPZVCSRTQS
     ''' if x.isalpha()])
 
-
-    for each in range(9,900,9):
+    for each in range(36,900,9):
         print('Poly-alphabetic Key length: %s' % each)
         run = Run(each, cipher_text)
         # run.start_simple_substitution()
